44319a268d0fcab4dc47e78d91e409f6efe4d85e
fernhadthaya <36783358+fernhadthaya@users.noreply.github.com>
2020-01-25 16:49:52 +0700

    splashScreen

--- views/Login/Login.js
+++ views/Login/Login.js
@@ -1,103 +1,155 @@
 import React from 'react';
-import{
-    Image,
-    StatusBar,
-    ScrollView,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    StyleSheet,
+import {
+    ActivityIndicator,
     Alert,
+    AsyncStorage,
+    Image, 
+    StatusBar, 
+    StyleSheet, 
+    ScrollView,
+    Text, 
+    TextInput, 
+    TouchableOpacity,
+    View, 
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import NetworkFailed from '../../component/NetworkFailed';
+import NotFound from '../../component/NotFound';
+
+import UserModel from '../../models/UserModel';
+
+var user_model = new UserModel
 
 export class Login extends React.Component {
     constructor(props) {
         super(props)
-        this.state ={
-            username: "admin",
-            password: "password1234"
+        this.state = {
+            loading: false,
+            alert: '',
+            username: '',
+            password: '',
         }
     }
+
     componentDidMount() {
-this._getLogin("Hadthaya Insuk");
+        AsyncStorage.getItem('user_data').then((user) => { return JSON.parse(user) }).then((user_data) => {
+            if (user_data != null) {
+                // this.setState({ 
+                //     username: user_data.user_username, 
+                //     password: user_data.user_password 
+                // });
+                this.props.navigation.navigate('Home')
+            }
+        });
     }
 
-_getLogin(param = 'Hello'){
-    Alert.alert("Funtion","_getLogin :"+param)
-}
-
+    _getLogin(){
+        if (this.state.username.length == 0) {
+            Alert.alert("แจ้งเตือน","กรุณาระบุบัญชีผู้ใช้");
+        }else if(this.state.password.length == 0){
+            Alert.alert("แจ้งเตือน","กรุณากรอกรหัสผ่าน");
+        }else{
+            this.setState({
+                loading: true,
+                alert: '',
+            }, () => {
+                user_model.getLogin(this.state.username,this.state.password).then((response) => {
+                    if (response == false) {
+                        this.setState({
+                            loading: false,
+                            alert: 'network-failed',
+                        });
+                    }else if (response.data.length == 0) {
+                        this.setState({
+                            loading: false,
+                            alert: 'not-found',
+                        });
+                    }else{
+                        AsyncStorage.setItem('user_data',JSON.stringify(response.data[0])).then(() => {
+                            this.props.navigation.navigate('Home')
+                        });
+                    }
+                });
+            });
+        }
+    }
+        
     render() {
-        return(
-            <ScrollView style={{ backgoundColor: "#00000" }}>
+        var display = [];
+        if (this.state.loading) {
+            display.push(
+                <View style={{ flexDirection: "row", justifyContent: "center", flex: 1, backgroundColor: '#25aae1', borderRadius: 2, padding: 10, }}>
+                    <ActivityIndicator size="small" color="#fff"/>
+                </View>
+            )
+        }else{
+            display.push(
+                <TouchableOpacity style={{ flex: 1, backgroundColor: '#25aae1', borderRadius: 2, padding: 10, }} onPress={() => this._getLogin()}>
+                    <Text style={[ styles.text_font, { alignSelf: "center", }]}>LOGIN</Text>
+                </TouchableOpacity>
+            )
+        }
+
+        if (this.state.alert == 'network-failed') {
+            display.push(<NetworkFailed/>);
+        }else if (this.state.alert == 'not-found') {
+            display.push(<NotFound/>);
+        }
+
+        return (
+            <ScrollView style={{ backgroundColor: "#010001", }}>
                 <StatusBar hidden={true} />
-                <View style={{ padding:36}}>
-                    <Image resizeMode = "contain" 
-                        source={require('../../images/logo.png')}
-                        style = {{ width: 200, height: 200, marginBottom:24, alignSelf:'center'}}
+                <View style={{ padding: 36, }}>
+                    <Image 
+                        resizeMode="contain"
+                        source={require('../../images/logo.png')} 
+                        style={{ width: 200, height: 200, marginTop: 54, marginBottom: 24, alignSelf: 'center', }}
                     >
-                        </Image>
-                    
-                    <View style={[ styles.row_underline, {marginBottom:16}]}>    
-                        <Icon name="email-outline" style={styles.login_icon}/>
+                    </Image>
+                    <View style={[ styles.row_underline, { marginBottom: 16, }]}>
+                        <Icon name="email-outline" style={styles.login_icon} />
                         <TextInput placeholder="Email address"
                             placeholderTextColor="#ADADAD"
-                            underlineColorAndroid='transparent'
-                            style={{ color: '#ff3399',flex:1, fontSize:16,paddingLeft:12}}
-                            onChangeText={
-                                (val)=>{
-                                    this.setState({
-                                        username:val 
-                                    })
-                                }
-                            }
+                            editable={!this.state.loading}
+                            underlineColorAndroid='transparent' 
+                            style={[ styles.text_font, { flex: 1, paddingLeft: 12, } ]}
                             value={this.state.username}
+                            onChangeText={(username) => { this.setState({ username }) }}
                         />
                     </View>
-                    <View  style={[ styles.row_underline, {marginBottom:16}]}>
-                    <Icon name="email-outline" style={styles.login_icon}/>
+                    <View style={[ styles.row_underline, { marginBottom: 24, }]}>
+                        <Icon name="lock-outline" style={styles.login_icon} />
                         <TextInput placeholder="Password"
                             placeholderTextColor="#ADADAD"
+                            editable={!this.state.loading}
                             underlineColorAndroid='transparent'
-                            style={{ flex: 1, fontSize: 16, color: '#ff6600', paddingL: 10}}
+                            style={[ styles.text_font, { flex: 1, paddingLeft: 12, } ]}
                             secureTextEntry={true}
-                            onChangeText={
-                                (val)=>{
-                                    this.setState({
-                                        password:val 
-                                    })
-                                }
-                            }
                             value={this.state.password}
-                            />
+                            onChangeText={(password) => { this.setState({ password }) }}
+                        />
                     </View>
-
-                    <TouchableOpacity style={{ flex: 1, backgroundColor: '#25aae1', borderRadius: 2, padding: 10, }}
-                    onPress ={() => {this._getLogin()}}
-                    >
-                        <Text style={{ alignSelf: "center", fontSize: 16, color: '#fff'}}>{this.props.btn}</Text>
-                    </TouchableOpacity>
+                    {display}
                 </View>
             </ScrollView>
         );
     }
 }
 
-
-// ตอนกำหนดค่า
 const styles = StyleSheet.create({
-    row_underline: {
-        flexDirection: "row",
-        borderBottomWidth: 1,
+    text_font:{
+        fontSize: 16,
+        color: '#fff',
+    },
+    row_underline:{
+        flexDirection: "row", 
+        borderBottomWidth: 1, 
         borderBottomColor: '#b6b6b6',
-        
     },
     login_icon:{
         alignSelf: 'center',
-        fontSize: 20,
-        color: '#ADADAD'
-    }	
+        fontSize: 20, 
+        color: '#ADADAD', 
+    },
 });
-
-// ตอนใช้งาน	<View style={styles.container} > 
